5211b7386e71b1c0ff4caa95a388427480466602
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-03-25 00:08:49 +0200

    getById method updated.

--- app/methods/getById.js
+++ app/methods/getById.js
@@ -2,12 +2,6 @@
 
 var Client = require('../../models/client');
 
-module.exports = function (params, cb) {
-  params = params || {};
-  
-  if (!params.id) {
-    return cb(new Error('id is missing'));
-  }
-  
-  Client.findOne({ _id: params.id }, cb);
+module.exports = function (id, cb) {
+  Client.findOne({ _id: id }, cb);
 };
